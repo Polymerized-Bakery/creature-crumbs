fe2dfab90c71a8a95ae56cb15000187093788950
Daedreus <drakosoldier298@gmail.com>
2024-07-20 12:11:40 -0500

    im tired..

--- src/main/java/com/daedreus/Bakery/CreatureCrumbs/Entrypoint.java
+++ src/main/java/com/daedreus/Bakery/CreatureCrumbs/Entrypoint.java
@@ -1,22 +1,25 @@
-package com.example;
+package com.daedreus.Bakery.CreatureCrumbs;
 
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Entrypoint implements ModInitializer {
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("crecru");
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		PolymerResourcePackUtils.addModAssets("crecru");
+		PolymerResourcePackUtils.markAsRequired();
+		LOGGER.info("Hello Fabric world! Its time to bake!");
 	}
 }
